b9e0bd6830bc97bcc987e66064c2fc182b02a0fc
LucasBoe <lucas@lumotex.de>
2020-10-25 15:17:51 +0100

    added cycling

---
+++ Assets/Scripts/SimpleBicycleController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SimpleBicycleController : MonoBehaviour
+{
+    public Transform poleFront;
+    public Rigidbody myRigidbody;
+
+    public float speed;
+    public float steeringSpeed;
+    public float maxSpeed;
+    public AnimationCurve curve;
+
+    float steeringAmount = 0;
+
+    private void Update()
+    {
+        if (myRigidbody != null)
+        {
+            myRigidbody.velocity = myRigidbody.velocity.magnitude * GetSteeringVector(addOwnRotation: true);
+
+            steeringAmount += Input.GetAxis("Horizontal") * Time.deltaTime * steeringSpeed;
+            poleFront.localRotation = Quaternion.Euler(0, steeringAmount, 0);
+
+            myRigidbody.AddForce(GetSteeringVector() * Time.deltaTime * Input.GetAxis("Vertical") * speed * GetAccelorationFactor());
+
+            float rotation = transform.rotation.eulerAngles.y + steeringAmount * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, rotation, steeringAmount * 0.1f);
+            steeringAmount /= (1 + Time.deltaTime);
+        }
+    }
+    private Vector3 GetSteeringVector (bool addOwnRotation = false)
+    {
+        if (poleFront != null)
+        {
+            Vector3 steering = new Vector3(poleFront.forward.x, 0, poleFront.forward.z).normalized;
+            Vector3 forward = transform.forward.normalized;
+
+            if (addOwnRotation)
+            {
+                return ((steering + forward) / 2).normalized * -1;
+            }
+            else
+            {
+                return (steering) * -1;
+            }
+        }
+
+        return Vector3.forward;
+    }
+
+    private float GetAccelorationFactor ()
+    {
+        if (myRigidbody != null && curve != null)
+        {
+            return curve.Evaluate(myRigidbody.velocity.magnitude / maxSpeed);
+        }
+
+        return 1;
+    }
+
+    private void OnGUI()
+    {
+        string str = "";
+        str += myRigidbody.velocity.magnitude.ToString() + "\n";
+
+        if (myRigidbody != null && curve != null)
+        {
+            float positionOnCurve = myRigidbody.velocity.magnitude / maxSpeed;
+            str += "pos: " + positionOnCurve + " acel: " + curve.Evaluate(myRigidbody.velocity.magnitude / maxSpeed);
+        }
+
+        GUI.TextArea(new Rect(10,10,100,100),str);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position + GetSteeringVector(),0.1f);
+    }
+}
---
+++ Assets/Scripts/SimpleCameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public bool smooth = true;
+    public float smoothAmount = 0.2f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.position + target.forward * 3 + Vector3.up * 2.5f;
+
+        // Smoothly move the camera towards that target position
+        if (smooth)
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothAmount);
+        else
+            transform.position = targetPosition;
+
+        transform.forward = (target.position + Vector3.up * 1.5f) - transform.position;
+    }
+}
