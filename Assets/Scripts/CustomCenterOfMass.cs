a72f2b7cb8cd61998c6e0ec488e4da9cde6646f3
LucasBoe <lucas@lumotex.de>
2020-10-28 13:06:17 +0100

    worked on physics for some objects

---
+++ Assets/Scripts/CustomCenterOfMass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCenterOfMass : MonoBehaviour
+{
+    public Vector3 customCenterOfMass;
+    private void Start()
+    {
+        Debug.Log(transform.rotation);
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody != null)
+            rigidbody.centerOfMass = customCenterOfMass;
+    }
+}
